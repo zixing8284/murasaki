a4a4942b5c6ff95aebd3bc062308a25fd9082187
sasasa10 <zixing8284@gmail.com>
2026-02-09 15:22:56 +0800

    Del demo files

--- packages/ui/src/components/button/demos/basic.tsx
+++
@@ -1,5 +0,0 @@
-import { Button } from '../button'
-
-export function Basic(): React.ReactElement {
-  return <Button>OK</Button>
-}
--- packages/ui/src/components/checkbox/demos/basic.tsx
+++
@@ -1,5 +0,0 @@
-import { Checkbox } from '../checkbox'
-
-export function Basic(): React.ReactElement {
-  return <Checkbox name="demo-basic">Enable feature</Checkbox>
-}
